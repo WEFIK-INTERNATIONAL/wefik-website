2fffab857917d1c6ec049e110e4dcec52dce92f9
Sandip123samanta <samanthasandip565@gmail.com>
2025-08-31 11:47:20 +0530

    fix: fixed styling bugs

--- src/components/common/AppLayoutClient.jsx
+++ src/components/common/AppLayoutClient.jsx
@@ -1,20 +1,27 @@
 "use client";
-
 import React, { useRef } from "react";
+import { usePathname } from "next/navigation";
 import NavBar from "./navbar/NavBar";
 import LenisProvider from "./LenisProvider";
+import BackgroundNoise from "@/components/common/BackgroundNoise";
 
 export default function AppLayoutClient({ children }) {
     const contentRef = useRef(null);
+    const pathname = usePathname();
+    const isAdmin = pathname.startsWith("/admin");
 
+    if (isAdmin) {
+        return <div className="min-h-screen">{children}</div>;
+    }
     return (
-        <>
+        <div className="min-h-screen bg-black text-white relative">
+            <BackgroundNoise />
             <LenisProvider>
                 <NavBar contentRef={contentRef} />
                 <div ref={contentRef} data-scroll-container>
                     {children}
                 </div>
             </LenisProvider>
-        </>
+        </div>
     );
 }
